5ee4ca6e0ef734e4fd4ce901bc15927ed22c66f7
Ankith Singh <151456379+ankith179@users.noreply.github.com>
2026-07-10 10:14:26 +0530

    Time: 608 ms (76.24%), Space: 19.4 MB (64.98%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total = sum(nums)
+        if total % 2 != 0:
+            return False
+            
+        haf = total // 2
+        dp = [False] * (haf + 1)
+        dp[0] = True
+        
+        for stone in nums:
+            for i in range(len(dp) - 1, stone - 1, -1):
+                if dp[i - stone]:
+                    dp[i] = True
+                    
+        return dp[haf]
